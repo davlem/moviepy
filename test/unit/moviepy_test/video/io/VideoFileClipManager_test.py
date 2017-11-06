07895a17550b7ecc0263858f0c4c1195ce4218ee
david <cordlg@yahoo.es>
2017-11-06 16:11:37 +0100

    VideoFileClipManagr threads.

--- test/unit/moviepy_test/video/io/VideoFileClipManager_test.py
+++ test/unit/moviepy_test/video/io/VideoFileClipManager_test.py
@@ -1,6 +1,11 @@
 """moviepy.video.io - VideoFileClipManager test module.
 """
 
+from thread import (
+    start_new_thread,
+    allocate_lock,
+    )
+
 import unittest2 as unittest
 from test.unit._core import (
     count_elapsed_time,
@@ -40,6 +45,11 @@ def get_test_tmp_path_for_file(filename):
         ))
 
 
+_nthreads = 0
+_thstart = False
+_result = False
+_lock = allocate_lock()
+
 class test_VideoFileClipManager(unittest.TestCase):
 
     def setUp(self):
@@ -54,6 +64,7 @@ class test_VideoFileClipManager(unittest.TestCase):
             (0, 1, 2, 3,),
             
             # method_join_big_videoclips_files
+            # test_method_join_big_videoclips_files_thread
             (
                 # ouput, video_list
                 (
@@ -70,6 +81,7 @@ class test_VideoFileClipManager(unittest.TestCase):
             (0, 1, 2, 3,),
 
             # method_join_big_videoclips_files
+            # test_method_join_big_videoclips_files_thread
             (
                 (True),
                 ),
@@ -118,6 +130,57 @@ class test_VideoFileClipManager(unittest.TestCase):
             self.assertEqual(_re, _r0)
 
         _me.close()
+
+
+    def test_method_join_big_videoclips_files_thread(self, ):
+        _in, _me = 1, _mo.VideoFileClipManager()
+
+        def _load_many_video_clips(list_):
+            for _e0 in list_:
+                _me.load_file_clip(
+                    get_test_media_path_for_file(_e0)
+                    )
+            return _me
+
+        def _make_video(_e0, ):
+            global _nthreads, _thstart, _result
+            _lock.acquire()
+            _nthreads += 1
+            _thstart = True
+            _result = False
+            _lock.release()
+
+            _me, _time = count_elapsed_time(
+                _load_many_video_clips,
+                ((_e0[1],), {}),
+                show=True
+                )
+            _re, _time = count_elapsed_time(
+                _me.write_videofile, 
+                ((get_test_tmp_path_for_file(_e0[0]),), {}),
+                show=True,
+                )
+
+            _lock.acquire()
+            _nthreads -= 1
+            _thstart = False
+            _result = _re
+            _lock.release()
+
+        for _n0, _e0 in enumerate(self._d0[_in]):
+            start_new_thread(
+                _make_video, (_e0,)
+                )
+            while not _thstart:
+                pass
+            while _nthreads > 0:
+                pass
+            # ~ import pdb; pdb.set_trace()
+            _r0 = self._r0[_in][_n0]
+            self.assertEqual(_result, _r0)
+
+        _me.close()
+
 
 
 if __name__=='__main__':
